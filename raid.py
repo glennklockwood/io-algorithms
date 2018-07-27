35d81f0fe4afc7eb916320f9a691d797a9a38704
Glenn K. Lockwood <glock@lbl.gov>
2018-07-27 15:56:13 -0700

    working RAID5 implementation; added validation test

--- raid.py
+++ raid.py
@@ -75,12 +75,12 @@ class Raid5Stripe(object):
         """
         calculated_block = None
         for block in self.blocks[0:block_id] + self.blocks[block_id+1:]:
-            if calculated_block:
+            if calculated_block is not None:
                 calculated_block ^= block
             else:
                 calculated_block = block
 
-        return calculated_block
+        return calculated_block ^ self.parity
 
     def calculate_parity(self, old_block=None, new_block=None):
         """Calculate parity for the stripe
@@ -108,13 +108,42 @@ class Raid5Stripe(object):
                 parity ^= block
         return parity
 
-if __name__ == "__main__":
-    stripe = Raid5Stripe(width=3)
+def test_rebuild_stripe(reference_blocks=None, width=8):
+    """Ensure that parity is rebuilt correctly
+
+    Args:
+        reference_blocks (list of 8-bit ints or None): Blocks to use for raid
+            stripe
+        width (int): Number of blocks to randomly generate if reference_blocks
+            is None.  Otherwise ignored.
+    """
+
+    if reference_blocks is None:
+        import random
+        random.seed(a=0)
+        stripe = Raid5Stripe(width=width)
+        reference_blocks = [random.randint(0, 0b11111111) for x in range(width)]
+
+    # Set contents of each block in the RAID stripe
     print "                     parity is {:08b}".format(stripe.parity)
-    for block_id, contents in enumerate([0b11010111, 0b00100100, 0b01101110]):
+    for block_id, contents in enumerate(reference_blocks):
         stripe.write(block_id=block_id, contents=contents)
         print "Added {:08b} at %d; parity is {:08b}".format(stripe.blocks[block_id], stripe.parity) % block_id
 
+    # Recalculate every block based on parity
     for missing in range(stripe.width):
-        print "calculated: {:08b}".format(stripe.calculate_block(missing))
+        print_str = ""
+        for blockid in range(stripe.width):
+            if blockid == missing:
+                print_str += "???????? "
+            else:
+                print_str += "{:08b} ".format(stripe.blocks[blockid])
+        calculated_block = stripe.calculate_block(missing)
+        print print_str
+        print "parity:     {:08b}".format(stripe.parity)
+        print "calculated: {:08b}".format(calculated_block)
         print "vs          {:08b}".format(stripe.blocks[missing])
+        assert calculated_block == stripe.blocks[missing]
+
+if __name__ == "__main__":
+    test_rebuild_stripe()
